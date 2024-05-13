3707779c08f5259131075dc99b523a6767d3f9c3
Anna Wagner <annwagner8888@gmail.com>
2024-05-13 14:41:00 +0300

    FAK-42: Fix csm isMedicineMissing

--- src/constantly_stored_medicine/constantly_stored_medicine.service.ts
+++ src/constantly_stored_medicine/constantly_stored_medicine.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import {
-  Repository
-} from 'typeorm';
+import { Repository } from 'typeorm';
 import { Medicine } from '../medicine/entities/medicine.entity';
 import { MedicineService } from '../medicine/medicine.service';
 import { CreateConstantlyStoredMedicineDto } from './dto/create-constantly_stored_medicine.dto';
 import { UpdateConstantlyStoredMedicineDto } from './dto/update-constantly_stored_medicine.dto';
 import { ConstantlyStoredMedicine } from './entities/constantly_stored_medicine.entity';
-import { TConstantlyStoredMedicine } from "./types/constantly_stored_medicine";
+import {
+  MedicinePriorityEnum,
+  TConstantlyStoredMedicine
+} from "./types/constantly_stored_medicine";
 
 @Injectable()
 export class ConstantlyStoredMedicineService {
@@ -30,13 +31,30 @@ export class ConstantlyStoredMedicineService {
     
     return await this.constantlyStoredMedicineRepository.findOneBy({ id } );
   }
+  
+  isMedicineMissingByName(csm, medicines) {
+    return !medicines.some(medicine => medicine.name.toLowerCase() === csm.name.toLowerCase());
+  }
+  
+  isMedicineMissingByCategory(csm, medicines) {
+    return !medicines.some(medicine => {
+      const categoryIds = medicine.categories.map(m => m.id);
+    
+      return csm.categories.every(c => categoryIds.includes(c.id));
+    });
+  }
+  isMedicineMissing(csm, medicines) {
+    if ( csm.priority === MedicinePriorityEnum.CATEGORY ) return this.isMedicineMissingByCategory(csm, medicines);
+  
+    return this.isMedicineMissingByName(csm, medicines);
+  }
   async findAll(): Promise<TConstantlyStoredMedicine[]> {
     const csm = await this.constantlyStoredMedicineRepository.find({order: { name: "ASC" }});
     const medicines = await this.medicineService.findAll({});
-
+  
     return csm.map(item => ({
       ...item,
-      isMissing:!medicines.some(m => (m.name === item.name) && (m.description === item.description))
+      isMissing: this.isMedicineMissing(item, medicines)
     })).sort(this.compareMedicine);
   }
 
--- src/constantly_stored_medicine/types/constantly_stored_medicine.ts
+++ src/constantly_stored_medicine/types/constantly_stored_medicine.ts
@@ -1,3 +1,8 @@
 import { ConstantlyStoredMedicine } from '../entities/constantly_stored_medicine.entity';
 
 export type TConstantlyStoredMedicine = ConstantlyStoredMedicine & { isMissing: boolean };
+
+export enum MedicinePriorityEnum {
+  NAME = 'name',
+  CATEGORY = 'category'
+}
